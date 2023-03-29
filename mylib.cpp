539a00a72681eb0c36023f6e53113a9465a6f73d
Pranas Zeromskas <76076765+Zeromskas@users.noreply.github.com>
2023-03-29 22:07:53 +0300

    minor optimizing changes

--- mylib.cpp
+++ mylib.cpp
@@ -113,7 +113,7 @@ void generuotiAtsitiktinius(vector<Studentas> &studentai)
     Pazymiai pazymiai;
     pazymiai.nd.reserve(pazymiuKiekis);
 
-    for (int i = 0; i < studentuKiekis; i++)
+    for (int i = 0; i < studentuKiekis; ++i)
     {
         temp.vardas = "vardas" + to_string(i);
         temp.pavarde = "pavarde" + to_string(i);
@@ -127,7 +127,7 @@ void generuotiAtsitiktinius(vector<Studentas> &studentai)
     }
 }
 
-void failoSkaitymas(vector<Studentas> &studentai, string filename)
+void failoSkaitymas(vector<Studentas> &studentai, string const &filename)
 {
     ifstream in(filename);
 
@@ -150,7 +150,7 @@ void failoSkaitymas(vector<Studentas> &studentai, string filename)
     while (in >> temp.vardas)
     {
         in >> temp.pavarde;
-        for (int i = 0; i < pazymiuKiekis; i++)
+        for (int i = 0; i < pazymiuKiekis; ++i)
         {
             in >> p;
             pazymiai.nd.push_back(p);
@@ -166,7 +166,7 @@ void failoSkaitymas(vector<Studentas> &studentai, string filename)
     // {
     //     std::stringstream ss(line);
     //     ss >> temp.vardas >> temp.pavarde;
-    //     for (int i = 0; i < pazymiuKiekis; i++)
+    //     for (int i = 0; i < pazymiuKiekis; ++i)
     //     {
     //         ss>>p;
     //         pazymiai.nd.push_back(p);
@@ -181,7 +181,7 @@ void failoSkaitymas(vector<Studentas> &studentai, string filename)
     in.close();
 }
 
-void rikiavimas(vector<Studentas> &studentai, string sortType)
+void rikiavimas(vector<Studentas> &studentai, string const &sortType)
 {
     cout<<"Duomenys rikiuojami pagal "<<sortType<<endl;
     if (sortType == "name")
@@ -190,7 +190,7 @@ void rikiavimas(vector<Studentas> &studentai, string sortType)
         sort(studentai.begin(), studentai.end(), compareGrade);
 }
 
-void spausdinimas(vector<Studentas> &studentai, string filename)
+void spausdinimas(vector<Studentas> &studentai, string const &filename)
 {
     cout<<"Duomenys išvedami..."<<endl;
 
@@ -203,7 +203,7 @@ void spausdinimas(vector<Studentas> &studentai, string filename)
     out << oss->str();
     oss->str("");
 
-    for (int i = 0; i < studentai.size(); i++)
+    for (int i = 0; i < studentai.size(); ++i)
     {
         (*oss) << setw(15) << left << studentai[i].vardas << setw(20) << left << studentai[i].pavarde << setw(18) << left << setprecision(3) << studentai[i].vidurkis << setw(18) << left << setprecision(3) << studentai[i].mediana << endl;
         if ((i + 1) % 10 == 0 || i + 1 == studentai.size())
